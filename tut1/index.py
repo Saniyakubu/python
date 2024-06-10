a49c751015ce36c50cb2e637daadd77fa9a0c544
sani yakubu <ysani095@gmail.com>
2024-06-10 11:06:29 +0800

    first commit

---
+++ tut1/index.py
@@ -0,0 +1,56 @@
+line01 = '***********************'
+line02 = '*                     *'
+line03 = '*       WELCOME       *'
+line01 = '***********************'
+
+## 01 if else statement
+
+number = 1
+
+# if number > 5:
+#     print('number is greater than 5')
+# else:
+#     print('number is not greater than 5')
+
+# ternary operator
+# print('number > 5') if number > 5 else print('number is < or = 5')
+
+
+## 02 Data types
+
+# literal assignment
+
+# name = 'sani'
+
+# print(type(name) == str)
+# print(type(name))
+# print(isinstance(name, str))
+
+# constructor function
+# pizza = str('pepperoni')
+
+# print(type(pizza) == str)
+# print(type(pizza))
+# print(isinstance(pizza, str))
+
+year = str(2000)
+
+firstName = 'sani'
+lastName = 'yakubu'
+# print('my name is ' + firstName + ' ' + lastName + ' and i like music from the ' + year + '\'s')
+# print(firstName.replace('sani', 'baso'))
+
+# print('')
+# print("Menu".center(20, "="))
+# print('')
+# print("Coffee".ljust(20, ".") + "$1".rjust(4))
+# print("Biscuit".ljust(20, ".") + "$5".rjust(4))
+# print("Ice cream".ljust(20, ".") + "$15".rjust(4))
+
+# myvalue = True
+# print(isinstance(myvalue, bool))
+# print(type(myvalue))
+
+import math
+
+print(math.pi)
